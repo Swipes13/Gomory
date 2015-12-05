76aab33d99bcecfcb62a6968487d2fc534f33a8c
Valentin <Ualentin92@gmail.com>
2015-12-06 03:40:22 +0400

    csharp start

---
+++ LinearProgrammingGUI/src/Form1.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace LinearProgrammingGUI {
+  partial class LPForm {
+    /// <summary>
+    /// Требуется переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Обязательный метод для поддержки конструктора - не изменяйте
+    /// содержимое данного метода при помощи редактора кода.
+    /// </summary>
+    private void InitializeComponent() {
+      this.SuspendLayout();
+      // 
+      // LPForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(689, 377);
+      this.Name = "LPForm";
+      this.ShowIcon = false;
+      this.Text = "Linear Programming";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+  }
+}
+
